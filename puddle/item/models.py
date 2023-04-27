02198b11eee815e424ec631fef23c3b1f2d2599f
Mateus <neveszalves@gmail.com>
2023-04-27 11:37:10 -0300

    added itens  and media in the database

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
     
 
 class Item(models.Model):
-    category = models.ForeignKey(Category,related_name='items',on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='items',on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
@@ -23,3 +23,7 @@ class Item(models.Model):
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from core.views import contact, index
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
-from core.views import index, contact
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contact/', contact, name='contact')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
