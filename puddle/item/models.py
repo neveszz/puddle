13091b3f701d7c2dbe62fc3c954ae8485713eb4b
Mateus <neveszalves@gmail.com>
2023-04-29 16:35:55 -0300

    Added quanty to items

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -19,6 +19,7 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    quantidade = models.IntegerField(blank=True, null=True)
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
